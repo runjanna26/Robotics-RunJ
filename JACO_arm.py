656b823b3aed6b184064ec0f0149b19c1d66d377
Run Janna <108509931+runjanna26@users.noreply.github.com>
2023-06-14 17:35:45 +0700

    update

--- JACO_arm.py
+++ JACO_arm.py
@@ -31,28 +31,6 @@ def dh_to_transformation_matrices(dh_parameters):
     """
     num_joints = len(dh_parameters)
     transformation_matrices = []
-
-    # alpha, a, d, theta = dh_parameters[0]
-    # T01 = dh_transform(alpha, a, d, theta)
-    # alpha, a, d, theta = dh_parameters[1]
-    # T12 = dh_transform(alpha, a, d, theta)
-    # alpha, a, d, theta = dh_parameters[2]
-    # T23 = dh_transform(alpha, a, d, theta)
-    # alpha, a, d, theta = dh_parameters[3]
-    # T34 = dh_transform( alpha, a, d, theta)
-    # alpha, a, d, theta = dh_parameters[4]
-    # T45 = dh_transform( alpha, a, d, theta)
-    # alpha, a, d, theta = dh_parameters[5]
-    # T56 = dh_transform( alpha, a, d, theta)
-
-    # T01 = T01
-    # T02 = T01@T12
-    # T03 = T01@T12@T23
-    # T04 = T01@T12@T23@T34
-    # T05 = T01@T12@T23@T34@T45
-    # T06 = T01@T12@T23@T34@T45@T56
-    # transformation_matrices = [T01,T02,T03,T04,T05,T06]
-
     previous_matrix = np.identity(4)
 
     for i in range(num_joints):
@@ -64,22 +42,6 @@ def dh_to_transformation_matrices(dh_parameters):
 
     return transformation_matrices
 
-# def dh_to_transformation_matrix(dh_parameters):
-#     """
-#     Convert DH parameters of a robot arm to the transformation matrix.
-#     :param dh_parameters: List of DH parameter tuples (alpha, a, d, theta)
-#     :return: 4x4 transformation matrix
-#     """
-#     num_joints = len(dh_parameters)
-#     transformation_matrix = np.identity(4)
-
-#     for i in range(num_joints):
-#         alpha, a, d, theta = dh_parameters[i]
-#         matrix = dh_transform(alpha, a, d, theta)
-#         transformation_matrix = np.dot(transformation_matrix, matrix)
-
-    return transformation_matrix
-
 def plot_robot_arm(transformation_matrices):
     """
     Plot the robot arm using the transformation matrices.
@@ -90,29 +52,22 @@ def plot_robot_arm(transformation_matrices):
 
     # Base coordinates
     base_coords = np.array([0, 0, 0, 1])
-
+    print(base_coords)
     # Plot each link and joint of the robot arm
     for matrix in transformation_matrices:
         link_coords = np.matmul(matrix, base_coords)
+        print(link_coords)
         x = [base_coords[0], link_coords[0]]
         y = [base_coords[1], link_coords[1]]
         z = [base_coords[2], link_coords[2]]
         ax.plot(x, y, z, 'b')
-
+        ax.plot( 0.10323798, -0.409084,    0.70513714,'o')
         # Plot joint marker
         ax.plot([link_coords[0]], [link_coords[1]], [link_coords[2]], 'ro')
-
+        
         base_coords = link_coords
 
-    # # Add a circular plane at the origin
-    # theta = np.linspace(0, 2*np.pi, 100)
-    # r = 0.1  # Radius of the circular plane
-    # x_circle = r * np.cos(theta)
-    # y_circle = r * np.sin(theta)
-    # z_circle = np.zeros_like(theta)
-    # ax.plot(x_circle, y_circle, z_circle, 'g')
     ax.plot(0, 0, 0, 'go')
-    
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
@@ -120,30 +75,30 @@ def plot_robot_arm(transformation_matrices):
     plt.show()
 
 ## DH variable [Solution 1]
-D1 = 0.2755
-D2 = 0.41
-e2 = 0.0098
-D3 = 0.2073
-D4 = 0.0741
-D5 = 0.0741
-D6 = 0.16
+D1 = 0.2755   # base to elbow
+D2 = 0.4100   # arm length
+D3 = 0.2073   # front arm length
+D4 = 0.0741   # first wrist length
+D5 = 0.0741   # second wrist length
+D6 = 0.1600   # wrist to center of the hand
+e2 = 0.0098   # joint 3-4 lateral offset
 
-aa = np.pi/6
-ca = cos(aa)
-sa = sin(aa)
-c2a = cos(2*aa)
-s2a = sin(2*aa)
-d4b = (D3+(D5*(sa/s2a)))
-d5b = (D4*(sa/s2a)) + (D5*(sa/s2a))
-d6b = (D5*(sa/s2a)) + (D6)
+aa = 30*np.pi/180
+ca = np.cos(aa)
+sa = np.sin(aa)
+c2a = np.cos(2*aa)
+s2a = np.sin(2*aa)
+d4b = D3 + sa/s2a*D4
+d5b = (sa/s2a*D4 + sa/s2a*D5)
+d6b = (sa/s2a*D5 + D6)
 
-# 0 15 180 -130 0 55 90
-q1_robot = 0
-q2_robot = 15
-q3_robot = 180
-q4_robot = -130
-q5_robot = 0
-q6_robot = 55
+# [261.74, 171.01, 67.6, 152.93, 37.12, 6.91]
+q1_robot = 261.74
+q2_robot = 171.01
+q3_robot = 67.6
+q4_robot = 152.93
+q5_robot = 37.12
+q6_robot = 6.91
 
 q1 = -q1_robot
 q2 =  q2_robot - 90
@@ -158,6 +113,7 @@ q3 = np.deg2rad(q3)
 q4 = np.deg2rad(q4)
 q5 = np.deg2rad(q5)
 q6 = np.deg2rad(q6)
+
 
 # Example DH parameters for a 7-DOF JACO arm
 # (alpha, a, d, theta)
@@ -175,5 +131,5 @@ dh_parameters = [
 transformation_matrix = dh_to_transformation_matrices(dh_parameters)
 
 # Print the resulting transformation matrix
-# print(transformation_matrix)
+print(transformation_matrix[5][0:3,3])
 plot_robot_arm(transformation_matrix)

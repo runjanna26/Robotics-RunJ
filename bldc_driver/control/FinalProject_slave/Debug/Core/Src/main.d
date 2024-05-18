eef6390870893222af5fcb63088bc695eb42ee84
Run Janna <108509931+runjanna26@users.noreply.github.com>
2024-05-19 02:32:40 +0700

    push gitignore

--- bldc_driver/control/FinalProject_slave/Debug/Core/Src/main.d
+++ bldc_driver/control/FinalProject_slave/Debug/Core/Src/main.d
@@ -28,7 +28,7 @@ Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
- C:/Users/WINDOWS\ 11/Documents/Robotics-RunJ/bldc_driver/control/FinalProject_slave/Core/Libs/HelloRunJ.h
+ /Users/runj/Documents/GitHub/Robotics-RunJ/bldc_driver/control/FinalProject_slave/Core/Libs/HelloRunJ.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -59,4 +59,4 @@ Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
-C:/Users/WINDOWS\ 11/Documents/Robotics-RunJ/bldc_driver/control/FinalProject_slave/Core/Libs/HelloRunJ.h:
+/Users/runj/Documents/GitHub/Robotics-RunJ/bldc_driver/control/FinalProject_slave/Core/Libs/HelloRunJ.h:

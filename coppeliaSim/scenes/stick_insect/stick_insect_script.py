fb0090a93bd0b01eac4c1f3fea1cd0138f58c1af
refine_2 <runjanna26@github.com>
2025-02-24 00:56:24 +0700

    update sim stick insect real path

--- coppeliaSim/scenes/stick_insect/stick_insect_script.py
+++ coppeliaSim/scenes/stick_insect/stick_insect_script.py
@@ -1,9 +1,18 @@
 import numpy as np
+import pandas as pd
+
 
 def sysCall_init():
     sim = require('sim')
 
     global stick_insect_joint, stick_insect_foot_force
+    global leg_0_df, leg_1_df, leg_2_df
+    leg_0_df = pd.read_csv('../../Robotics-RunJ/coppeliaSim/scenes/stick_insect/include/stick_insect_path/rbfn_joint_targets_0.csv')
+    leg_1_df = pd.read_csv('../../Robotics-RunJ/coppeliaSim/scenes/stick_insect/include/stick_insect_path/rbfn_joint_targets_1_v1.csv')
+    leg_2_df = pd.read_csv('../../Robotics-RunJ/coppeliaSim/scenes/stick_insect/include/stick_insect_path/rbfn_joint_targets_2_new_v1.csv')
+    
+
+        
 
     stick_insect_joint      = {'TR': [],'CR': [],'FR': [],'TL': [],'CL': [],'FL': []}
     stick_insect_foot_force = {'R': [], 'L': []}
@@ -20,20 +29,38 @@ def sysCall_init():
     # print(stick_insect_joint)
     # print(stick_insect_foot_force)
     
-    target_angles = {'TR': [30, 0, -40],
+    initial_joint_angles = {'TR': [30, 0, -40],
                      'CR': [10, 0,  10],
                      'FR': [-60, -60, -60],
                      'TL': [ 30,   0, -40],
                      'CL': [ 10,   0,  10],
                      'FL': [-60, -60, -60]}
-    for group, angles in target_angles.items():   # Set joint target positions using a loop
+    for group, angles in initial_joint_angles.items():   # Set joint target positions using a loop
         for i, angle in enumerate(angles):
             sim.setJointTargetPosition(stick_insect_joint[group][i], np.deg2rad(angle))
 
-    sim.setObjectInt32Parameter(stick_insect_body, sim.shapeintparam_static, 0)  
-
+    # sim.setObjectInt32Parameter(stick_insect_body, sim.shapeintparam_static, 0) 
+     
+i = 0
 def sysCall_actuation():
     pass
+    global i
+    
+    if i < len(leg_0_df['TR0']):
+        sim.setJointTargetPosition(stick_insect_joint['TR'][0], leg_0_df['TR0'][i])
+        sim.setJointTargetPosition(stick_insect_joint['CR'][0], leg_0_df['CR0'][i])
+        sim.setJointTargetPosition(stick_insect_joint['FR'][0], leg_0_df['FR0'][i])
+        
+        sim.setJointTargetPosition(stick_insect_joint['TR'][1], leg_1_df['TR1'][i])
+        sim.setJointTargetPosition(stick_insect_joint['CR'][1], leg_1_df['CR1'][i])
+        sim.setJointTargetPosition(stick_insect_joint['FR'][1], leg_1_df['FR1'][i])        
+        
+        sim.setJointTargetPosition(stick_insect_joint['TR'][2], leg_2_df['TR2'][i])
+        sim.setJointTargetPosition(stick_insect_joint['CR'][2], leg_2_df['CR2'][i])
+        sim.setJointTargetPosition(stick_insect_joint['FR'][2], leg_2_df['FR2'][i]) 
+        i += 1
+    else:
+        i = 0
 
 def sysCall_sensing():
     pass
